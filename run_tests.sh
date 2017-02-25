57983e18352e1f8894da13b9f1db0c9887c8ad3b
TedaLIEz <aliezted@gmail.com>
2017-02-25 13:29:39 +0800

    project: edit shell script

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,6 @@ if [ $retval -ne 0 ]; then
     exit $retval
 fi
 if [ ${TEST} == "android" ]; then
-    echo no | android create avd --force --name test --target $ANDROID_TARGET --abi $ANDROID_ABI --sdcard 800M
-    emulator -memory 8000 -avd test -no-audio -cache-size 400 -netdelay none -netspeed full -no-window &
-    android-wait-for-emulator
     adb devices
     adb shell svc power stayon true
     sleep 10
@@ -18,7 +15,24 @@ if [ ${TEST} == "android" ]; then
     adb shell settings put global window_animation_scale 0.0 
     adb shell settings put global transition_animation_scale 0.0
     adb shell settings put global animator_duration_scale 0.0
-    ./gradlew connectedAndroidTest -PdisablePreDex --stacktrace -i
+    ./gradlew build assembleAndroidTest
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo "error on assembling, exit code: "$retval
+      exit $retval
+    fi
+    ./gradlew :parsingplayer:installDebugAndroidTest
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo "error on install, exit code: "$retval
+      exit $retval
+    fi
+    adb shell am instrument -w -r -e package com.hustunique.parsingplayer -e debug false com.hustunique.parsingplayer.test/android.support.test.runner.AndroidJUnitRunner
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo "error in adb, exit code: "$retval
+      exit $retval
+    fi
 elif [${TEST} == "unit"]; then
     ./gradlew --stacktrace test
 fi
