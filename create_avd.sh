f8537506a30eaf34bc53e93fcc638c02a1fb842c
TedaLIEz <aliezted@gmail.com>
2017-02-25 20:48:08 +0800

    feat: disable debug mode

--- create_avd.sh
+++ create_avd.sh
@@ -2,7 +2,7 @@
 if [ ${TEST} == "android" ]; then
     echo no | android create avd --force --name test --target $ANDROID_TARGET --abi $ANDROID_ABI --sdcard 800M
     echo "hw.keyboard=yes" >> $HOME/.android/avd/test.avd/config.ini
-    emulator -avd test -debug all -memory 4000 -noskin -no-audio -cache-size 400 -netfast -no-window -no-boot-anim &
+    emulator -avd test -memory 4000 -noskin -no-audio -cache-size 400 -netfast -no-window -no-boot-anim &
     android-wait-for-emulator
     adb shell input keyevent 82 &
     cat $HOME/.android/avd/test.avd/config.ini
