2d37a3b9e00449e435a0b28d54dfad09e64e8137
TedaLIEz <aliezted@gmail.com>
2017-02-25 20:36:38 +0800

    disable net shaping

--- create_avd.sh
+++ create_avd.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 if [ ${TEST} == "android" ]; then
     echo no | android create avd --force --name test --target $ANDROID_TARGET --abi $ANDROID_ABI --sdcard 800M
-    echo "runtime.network.latency=none" >> $HOME/.android/avd/test.avd/config.ini
-    echo "runtime.network.speed=full" >> $HOME/.android/avd/test.avd/config.ini
     echo "hw.keyboard=yes" >> $HOME/.android/avd/test.avd/config.ini
-    emulator -avd test -debug all -memory 4000 -noskin -no-audio -cache-size 400 -netdelay none -netspeed full -no-window -no-boot-anim &
+    emulator -avd test -debug all -memory 4000 -noskin -no-audio -cache-size 400 -netfast -no-window -no-boot-anim &
     android-wait-for-emulator
     adb shell input keyevent 82 &
     cat $HOME/.android/avd/test.avd/config.ini
