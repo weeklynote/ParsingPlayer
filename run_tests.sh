cf90f48c0d2b86fa466f689e0755c81700519889
TedaLIEz <aliezted@gmail.com>
2017-02-27 00:35:47 +0800

    project: edit sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 echo "TESTING..."
 if [ ${TEST} == "android" ]; then
-    ./gradlew uninstallAll
-    ./gradlew installDebugAndroidTest
-#    retval=$?
-#    if [ $retval -ne 0 ]; then
-#      echo "error on install, exit code: "$retval
-#      exit $retval
-#    fi
-#    adb shell am instrument -w -r -e package com.hustunique.parsingplayer -e debug false com.hustunique.parsingplayer.test/android.support.test.runner.AndroidJUnitRunner
-    ./gradlew connectedAndroidTest
-#    retval=$?
-#    if [ $retval -ne 0 ]; then
-#      echo "error in espresso testing, exit code: "$retval
-#      exit $retval
-#    fi
+    ./gradlew --stacktrace connectedAndroidTest
 elif [${TEST} == "unit"]; then
     ./gradlew --stacktrace test
     retval=$?
