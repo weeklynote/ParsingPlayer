04c7680dc107ad17cbe57ac76563c9376a92606c
TedaLIEz <aliezted@gmail.com>
2017-02-27 10:20:58 +0800

    project: edit sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,11 +4,11 @@ if [ ${TEST} == "android" ]; then
     ./gradlew --stacktrace connectedAndroidTest
 elif [${TEST} == "unit"]; then
     ./gradlew --stacktrace test
-    retval=$?
-    if [ $retval -ne 0 ]; then
-        echo "TEST FAILED: " $retval
-        exit $retval
-    fi
+fi
+retval=$?
+if [ $retval -ne 0 ]; then
+   echo "TEST FAILED: " $retval
+   exit $retval
 fi
 echo "TEST DONE"
 
